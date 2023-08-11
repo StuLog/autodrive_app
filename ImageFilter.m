5203c034fc905bd7af2370ac9e51bbccff4b694c
Stuart Logan <93291680+StuLog@users.noreply.github.com>
2023-08-11 09:57:43 -0400

    Add files via upload

---
+++ ImageFilter.m
@@ -0,0 +1,32 @@
+
+% read the original image
+I = imread('TrafficLight.jpg');
+
+% call createMask function to get the mask and the filtered image
+[BW,maskedRGBImage] = createMask(I);
+
+% plot the original image, mask and filtered image all in one figure
+subplot(1,3,1);imshow(I);title('Original Image');
+subplot(1,3,3);imshow(maskedRGBImage);title('Filtered Image');
+
+function [BW,maskedRGBImage] = createMask(RGB) 
+    I = rgb2hsv(RGB);
+
+    %this is where we have the color selection
+    channel1Min = 0.970;
+    channel1Max = 0.138;
+    
+    %this is for saturation so the more vibrant the color is
+    channel2Min = 0.000;
+    channel2Max = 1.000;
+    
+    %this is the brightness
+    channel3Min = 0.800;
+    channel3Max = 1.000;
+    
+    BW = ( (I(:,:,1) >= channel1Min) | (I(:,:,1) <= channel1Max) ) & ...
+        (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
+        (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
+    maskedRGBImage = RGB;
+    maskedRGBImage(repmat(~BW,[1 1 3])) = 0;
+end
